8fd142e7192895c90d22a8640e21375fde16ee01
HardLost0 <64424993+HardLost0@users.noreply.github.com>
2021-07-15 11:39:41 -0500

    Add files via upload

---
+++ Alex_C_Structure_Exercise.playground/Contents.swift
@@ -0,0 +1,7 @@
+struct carStruct {
+    var cars1 = "V4"
+    var cars2 = "V6"
+    var cars3 = "V8"
+}
+let cars = carStruct()
+print("The possible car engines that you can select when buying a new car are \n\(cars.cars1), \(cars.cars2), \(cars.cars3)")
