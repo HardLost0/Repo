d89b9e0834c4fe814e425dadf65fc6456aad82ce
HardLost0 <64424993+HardLost0@users.noreply.github.com>
2021-07-19 11:47:11 -0500

    Add files via upload

---
+++ Alex_C_FunctionExercise.playground/Contents.swift
@@ -0,0 +1,8 @@
+class buyingCar {
+    var engine = "Engine"
+    var Color = "Color"
+    var Model = "Model"
+    var Year = "Year"
+}
+let car = buyingCar()
+print("The possible feature you could select when buying a car is a \(car.engine),\(car.Color),\(car.Model),\(car.Year)")
